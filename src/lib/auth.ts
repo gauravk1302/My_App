d9bac3d8c8ad9190d78eb308a940bc7c1031a4d9
gauravk1302 <gauravsaini77069@gamil.com>
2026-03-07 21:59:01 +0530

    feat: initial commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+ // relative path
+import { PrismaClient } from "@/generated/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma , {
+        provider: 'mongodb',
+    }), 
+    emailAndPassword : {
+        enabled:true,
+    },
+    secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL,
+})
